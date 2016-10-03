c84453ad0a6279c5dfed47d13c55c2a8ae37d643
Niklas Molnes Hole <nikasmh@hotmail.com>
2016-10-03 20:10:10 +0200

    Modified server.js and added webpack to prod dependencies

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express')
 var app = express()
 
 app.use(express.static(__dirname + '/images'))
+app.use(express.static(__dirname + '/dist'))
 app.use(express.static(__dirname + '/node_modules'))
 
 app.get('/', function (req, res) {
