07ab5ff73b0390543785057ae96ad28d5fe9d240
Niklas Molnes Hole <nikasmh@hotmail.com>
2016-10-03 21:01:03 +0200

    Made static folders in server config

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 var express = require('express')
 var app = express()
 
-app.use(express.static(__dirname + '/images'))
-app.use(express.static(__dirname + '/dist'))
-app.use(express.static(__dirname + '/node_modules'))
+app.use('/images', express.static(__dirname + '/images'))
+app.use('/dist', express.static(__dirname + '/dist'))
+app.use('/node_modules', express.static(__dirname + '/node_modules'))
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html')
