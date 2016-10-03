d42c731d52a21538000664a3cb9294a010422677
Niklas Molnes Hole <nikasmh@hotmail.com>
2016-10-03 20:21:54 +0200

    Replaced all double quotes with single

--- webpack.config.js
+++ webpack.config.js
@@ -8,27 +8,27 @@ module.exports = {
     publicPath: '/dist/',
     filename: 'bundle.js'
   },
-  devtool: "source-map",
+  devtool: 'source-map',
   resolve: {
-    extensions: ["", ".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
+    extensions: ['', '.webpack.js', '.web.js', '.ts', '.tsx', '.js']
   },
   module: {
     loaders: [
       {
         test: /\.tsx?$/,
         exclude: /node_modules/,
-        loader: "ts-loader"
+        loader: 'ts-loader'
       }
     ],
     preLoaders: [
       {
         test: /\.js$/,
-        loader: "source-map-loader"
+        loader: 'source-map-loader'
       }
     ]
   },
   externals: {
-    "react": "React",
-    "react-dom": "ReactDOM"
+    'react': 'React',
+    'react-dom': 'ReactDOM'
   }
 }
