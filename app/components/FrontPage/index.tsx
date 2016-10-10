e78f74432bcb8c9e19f8a44a8b07615f98abfdb5
Niklas Molnes Hole <nikasmh@hotmail.com>
2016-10-10 02:28:43 +0200

    Adding a new smooth design for the front page

--- app/components/FrontPage/index.tsx
+++ app/components/FrontPage/index.tsx
@@ -7,8 +7,16 @@ export class FrontPage extends React.Component<FrontPageProps, {}> {
     return (
       <div className="page page-front">
         <div className="front-splash">
-          <div className="front-splash-left">AscendNTNU</div>
-          <div className="front-splash-center"><img src="images/logo/logo-ascend-below.svg" /></div>
+          <div className="front-splash-left">
+            <p>
+              Autonomus aerial robotics for fun and learning.
+              Ascend NTNU is the
+              &nbsp;<a href="http://www.ntnu.edu/">Norwegian University Of Science and Technology's</a>
+              &nbsp;team in the
+              &nbsp;<a href="http://www.aerialroboticscompetition.org/">International Aerial Robotics Competition</a>.
+            </p>
+          </div>
+          <div className="front-splash-center"><img src="images/logo/logo-ascend-below-shadow.svg" /></div>
           <div className="front-splash-right"></div>
         </div>
       </div>
