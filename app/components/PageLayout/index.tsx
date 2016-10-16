8e0d66d671c44ccb7bb4e92609ba9b168444dbf6
Niklas Molnes Hole <nikasmh@hotmail.com>
2016-10-16 22:26:12 +0200

    Changed Section components and added option for additional classes

--- app/components/PageLayout/index.tsx
+++ app/components/PageLayout/index.tsx
@@ -3,14 +3,23 @@ import * as React from 'react'
 export class Title extends React.Component<{}, {}> {
   render() {
     return (
-      <h2 className="section-title">{this.props.children}</h2>
+      <h1 className="section-title">{this.props.children}</h1>
+    )
+  }
+}
+
+export class SubTitle extends React.Component<{}, {}> {
+  render() {
+    return (
+      <h2 className="sub-section-title">{this.props.children}</h2>
     )
   }
 }
 
 export interface SectionProps {
   title?: string,
-  theme?: string
+  theme?: string,
+  className?: string,
 }
 
 export class Section extends React.Component<SectionProps, {}> {
@@ -27,6 +36,9 @@ export class Section extends React.Component<SectionProps, {}> {
     }
 
     this.className = "section page-container"
+
+    if (this.props.className)
+      this.className += " " + this.props.className
 
     if (this.props.theme) {
       let theme: string = ""
@@ -54,12 +66,48 @@ export class Section extends React.Component<SectionProps, {}> {
   }
 }
 
-export interface SubSectionProps {}
+export interface SubSectionProps {
+  title?: string,
+  theme?: string,
+  className?: string,
+}
 
 export class SubSection extends React.Component<SubSectionProps, {}> {
+  public title: JSX.Element
+  public className: string
+
+  constructor(props: SectionProps) {
+    super(props)
+
+    if (this.props.title) {
+      this.title = (
+        <SubTitle>{this.props.title}</SubTitle>
+      )
+    }
+
+    this.className = "sub-section page-container"
+
+    if (this.props.className)
+      this.className += " " + this.props.className
+
+    if (this.props.theme) {
+      let theme: string = ""
+
+      switch (this.props.theme) {
+        case "dark":
+          theme = "dark"
+          break
+        default:
+          theme = "light"
+          break
+      }
+    }
+  }
+
   render() {
     return (
-      <div className="sub-section">
+      <div className={this.className}>
+        {this.title}
         {this.props.children}
       </div>
     )
