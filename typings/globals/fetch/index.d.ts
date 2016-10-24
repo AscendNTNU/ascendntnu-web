1d10788eca35673be0a6131cca6591d7d2563d10
Niklas Molnes Hole <nikasmh@hotmail.com>
2016-10-24 03:07:26 +0200

    Adding typings for TypeScript

---
+++ typings/globals/fetch/index.d.ts
@@ -0,0 +1,7 @@
+/// <reference path="../es6-promise/index.d.ts" />
+
+interface Window {
+    fetch(url: any, init?: any): Promise<any>;
+}
+
+declare var fetch: typeof window.fetch;
---
+++ typings/index.d.ts
@@ -0,0 +1,7 @@
+/// <reference path="globals/commonmark/index.d.ts" />
+/// <reference path="globals/es6-promise/index.d.ts" />
+/// <reference path="globals/fetch/index.d.ts" />
+/// <reference path="globals/react-dom/index.d.ts" />
+/// <reference path="globals/react-router/history/index.d.ts" />
+/// <reference path="globals/react-router/index.d.ts" />
+/// <reference path="globals/react/index.d.ts" />
