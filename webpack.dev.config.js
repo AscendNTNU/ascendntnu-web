dafefcd0b32f4f764a15539af407a49a01d1223c
Niklas Molnes Hole <nikasmh@hotmail.com>
2016-10-24 03:09:22 +0200

    Added json-loader to webpack configs

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -30,6 +30,10 @@ module.exports = {
       {
         test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
         loader: 'file'
+      },
+      {
+        test: /\.json$/,
+        loader: 'json'
       }
     ]
   },
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -40,6 +40,10 @@ module.exports = {
       {
         test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
         loader: 'file'
+      },
+      {
+        test: /\.json$/,
+        loader: 'json'
       }
     ]
   },
