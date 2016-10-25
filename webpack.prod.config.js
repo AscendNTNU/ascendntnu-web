694839b57413b002ec9d2a7dab17300bf9c24950
Niklas Molnes Hole <nikasmh@hotmail.com>
2016-10-25 21:17:19 +0200

    Fixed #24, fetch should hopefully work now

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -2,7 +2,7 @@ var path = require('path')
 var webpack = require('webpack')
 
 module.exports = {
-  entry: './app/index.tsx',
+  entry: ['whatwg-fetch', './app/index.tsx'],
   output: {
     path: path.join(__dirname, 'dist'),
     publicPath: '/dist/',
