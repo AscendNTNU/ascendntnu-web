2827c0a402ed7159035281e3b39b9eccd18ae5db
Niklas Molnes Hole <nikasmh@hotmail.com>
2016-10-31 00:38:25 +0100

    Added members from API to the TeamPage. First iteration

--- app/components/TeamPage/index.tsx
+++ app/components/TeamPage/index.tsx
@@ -1,16 +1,83 @@
 import * as React from 'react'
+import { Link } from 'react-router'
 
 import { Section, SubSection } from '../PageLayout'
 import { Breadcrumb } from '../Common/breadcrumb'
 
-export interface TeamPageProps {}
+export interface TeamPageProps {
+  params?: any & {
+    year?: number
+  }
+}
 
-export class TeamPage extends React.Component<TeamPageProps, {}> {
+export interface TeamPageState {
+  year: number,
+  members: any[],
+}
+
+export class TeamPage extends React.Component<TeamPageProps, TeamPageState> {
+  constructor(props: any){
+    super(props)
+
+    let year = 2017
+
+    if (props.params && props.params.year)
+      year = props.params.year
+
+    this.state = {
+      year: year,
+      members: []
+    }
+
+    this.getMembers(year)
+  }
+
+  componentWillReceiveProps (nextProps: any) {
+    let nextYear: number = parseInt(nextProps.params.year) || 2017
+    let year: number = parseInt(this.props.params.year) || this.state.year
+
+    if (nextYear != year) {
+      year = nextYear
+
+      this.state = {
+        year: year,
+        members: this.state.members
+      }
+
+      this.getMembers(year)
+    }
+  }
+
+  private changeTeam(evt: any) {
+    this.getMembers(parseInt(evt.target.dataset.year))
+  }
+
+  public getMembers (year: number) {
+    fetch("/api/v1/members/" + year).then(r => r.json()).then(r => {
+      this.setState({
+        year: year,
+        members: r
+      })
+    })
+  }
+
   render () {
+    let members = this.state.members.map((m: any, i: number) => {
+      return (
+        <div key={i}><b>{m.name}</b> - {m.role}</div>
+      )
+    })
+
     return (
       <div className="page page-team">
         <Breadcrumb routes={['team']} />
-        <Section title="Team page">
+        <Section title="The team">
+          We have five groups: Control, Perception, Planning, Hardware and Admin. The board consists of the project manager, the deputy project manager, the technical leader and the group leaders. This means that we formally have a quite hierarchical stucture. However, in practice we have a very flat structure where everybody contributes within the areas they want and where attention is needed.
+          <SubSection title="Members">
+            <Link to="team/2016"><button>2016</button></Link>
+            <Link to="team/2017"><button>2017</button></Link>
+            {members}
+          </SubSection>
         </Section>
       </div>
     )
