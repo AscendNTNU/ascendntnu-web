59197d6a6858bb72bc94b9ca8dff1d13d82b37c2
Niklas Molnes Hole <nikasmh@hotmail.com>
2016-11-05 02:39:27 +0100

    Added sponsors to frontpage

--- app/components/FrontPage/index.tsx
+++ app/components/FrontPage/index.tsx
@@ -3,16 +3,54 @@ import * as React from 'react'
 import Splash from './splash'
 import { Section, SubSection } from '../PageLayout'
 
-export interface FrontPageProps {}
+export interface FrontPageState {
+  year: number,
+  sponsors: any[],
+}
 
-export class FrontPage extends React.Component<FrontPageProps, {}> {
+export class FrontPage extends React.Component<{}, FrontPageState> {
+  constructor (props: any) {
+    super(props)
+
+    this.state = {
+      year: 2016,
+      sponsors: []
+    }
+
+    this.getSponsors(2016)
+  }
+
+  public getSponsors (year: number) {
+    fetch("/api/v1/sponsors/" + year).then(r => r.json()).then(r => {
+      this.setState({
+        year: year,
+        sponsors: r
+      })
+    })
+  }
+
   render () {
+    let sponsors = this.state.sponsors.map((sponsor: any, i: number) => {
+      console.log(sponsor)
+      return (
+        <div key={i} className="sponsor">
+          <a href={sponsor.link} className="sponsor-link">
+            <img src={sponsor.logo} title={sponsor.name} />
+          </a>
+          <div className="sponsor-text">
+            {sponsor.short_text}
+          </div>
+        </div>
+      )
+    })
+
     return (
       <div className="page page-front">
         <Splash />
-        <Section title="Our sponsors">
-          <p>Sponsor 1</p>
-          <p>Sponsor 2</p>
+        <Section title="Our sponsors" className="centered">
+          <div className="sponsors">
+            {sponsors}
+          </div>
         </Section>
       </div>
     )
