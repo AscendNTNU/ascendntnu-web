ba3186a0908eef2d8a33c32e743fb4c25384dbd5
Niklas Molnes Hole <nikasmh@hotmail.com>
2016-11-06 22:38:18 +0100

    Added a sponsorpage and a reuseable sponsorlist

---
+++ app/components/Common/sponsorlist.tsx
@@ -0,0 +1,55 @@
+import * as React from 'react'
+
+export interface SponsorListProps {
+  year?: number,
+}
+
+export interface SponsorListState {
+  year?: number,
+  sponsors: any[],
+}
+
+export class SponsorList extends React.Component<SponsorListProps, SponsorListState> {
+  constructor (props: any) {
+    super(props)
+
+    this.state = {
+      year: props.year || 2016,
+      sponsors: []
+    }
+
+    this.getSponsors(this.state.year)
+  }
+
+  public getSponsors (year: number) {
+    fetch("/api/v1/sponsors/" + year).then(r => r.json()).then(r => {
+      this.setState({
+        year: year,
+        sponsors: r
+      })
+    })
+  }
+
+  render() {
+    let sponsors = this.state.sponsors.map((sponsor: any, i: number) => {
+      return (
+        <div key={i} className="sponsor">
+          <a href={sponsor.link} className="sponsor-link">
+            <img src={sponsor.logo} title={sponsor.name} />
+          </a>
+          <div className="sponsor-text">
+            {sponsor.short_text}
+          </div>
+        </div>
+      )
+    })
+
+    return (
+      <div className="sponsors">
+        {sponsors}
+      </div>
+    )
+  }
+}
+
+export default SponsorList
--- app/components/SponsorPage/index.tsx
+++ app/components/SponsorPage/index.tsx
@@ -2,15 +2,27 @@ import * as React from 'react'
 
 import { Section, SubSection } from '../PageLayout'
 import { Breadcrumb } from '../Common/breadcrumb'
+import { SponsorList } from '../Common/sponsorlist'
 
-export interface SponsorPageProps {}
+export interface SponsorPageState {
+  year: number,
+}
 
-export class SponsorPage extends React.Component<SponsorPageProps, {}> {
+export class SponsorPage extends React.Component<{}, SponsorPageState> {
   render () {
+    let title: any = (
+      <span>Our sponsors <select>
+        <option value="2016">2016</option>
+        <option value="2017">2017</option>
+      </select>
+      </span>
+    )
+
     return (
       <div className="page page-sponsors">
         <Breadcrumb routes={['sponsors']} />
-        <Section title="Our sponsors" className="centered">
+        <Section title={title} className="centered">
+          <SponsorList year={2016} />
         </Section>
       </div>
     )
