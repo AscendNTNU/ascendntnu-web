12dfe1b78369f9485f3176c84fb2e83ad432a39b
Niklas Molnes Hole <nikasmh@hotmail.com>
2017-01-29 15:49:36 +0100

    Added a model drone to drone-page with spesified cam angle

--- app/components/DronePage/index.tsx
+++ app/components/DronePage/index.tsx
@@ -2,10 +2,14 @@ import * as React from 'react'
 
 import { Section, SubSection } from '../PageLayout'
 import { Breadcrumb } from '../Common/breadcrumb'
+import { ModelRenderer } from '../Common/model'
 
 export interface DronePageProps {}
+export interface DronePageState {
+  droneImages: any
+}
 
-export class DronePage extends React.Component<DronePageProps, {}> {
+export class DronePage extends React.Component<DronePageProps, DronePageState> {
   drones: any[]
   constructor () {
     super()
@@ -17,11 +21,19 @@ export class DronePage extends React.Component<DronePageProps, {}> {
         }
       },
       {
+        models: ['images/drones/drone2.stl'],
         style: {
           backgroundImage: 'url(/images/drones/drone2-minimized.jpg)'
         }
       }
     ]
+
+    this.state = {
+      droneImages: [
+        (<div className="drone-image" style={this.drones[0].style}></div>),
+        (<div className="drone-model"><ModelRenderer models={this.drones[1].models} process={[-80, 160, 160]} /></div>)
+      ]
+    }
   }
 
   render () {
@@ -30,13 +42,13 @@ export class DronePage extends React.Component<DronePageProps, {}> {
         <Breadcrumb routes={['drone']} />
         <Section className="row">
           <SubSection title="Drone 1" className="drone-container">
-            <div className="drone-image" style={this.drones[0].style}></div>
+            {this.state.droneImages[0]}
             <p className="drone-text">
               Our first aerial robot was born last fall. Creating it, flying it and using it has been a great learning experience for the team. The drone is however quite small, and we needed an upgrade in order to carry all the desired equipment.
             </p>
           </SubSection>
           <SubSection title="Drone 2" className="drone-container">
-            <div className="drone-image" style={this.drones[1].style}></div>
+            {this.state.droneImages[1]}
             <p className="drone-text">
               Our second aerial robot is custom designed using carbon fiber and 3D-printed parts. It was build for the 2016 IARC and designed with Mission 7 in mind, and it is able to carry all the equipment we need to herd the target robots across the green line.
             </p>
