94bfba0f38157d31d142e172fd38f9f799fe8da8
Niklas Molnes Hole <nikasmh@hotmail.com>
2017-02-08 00:28:39 +0100

    Added timeline to the frontpage

--- app/components/FrontPage/index.tsx
+++ app/components/FrontPage/index.tsx
@@ -2,6 +2,7 @@ import * as React from 'react'
 
 import Splash from './splash'
 import SponsorList from '../Common/sponsorlist'
+import { HistoryViewer } from '../Common/historyViewer'
 import { Section, SubSection } from '../PageLayout'
 
 export interface FrontPageState {
@@ -13,6 +14,8 @@ export class FrontPage extends React.Component<{}, FrontPageState> {
     return (
       <div className="page page-front">
         <Splash />
+        <Section title="History of Ascend NTNU" className="centered" />
+        <HistoryViewer />
         <Section title="Our sponsors" className="centered">
           <SponsorList />
         </Section>
