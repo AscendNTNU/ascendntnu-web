21131fd1adfa2f9e49fa9c17c77133aebc16fbdd
Niklas Molnes Hole <nikasmh@hotmail.com>
2017-02-21 02:34:23 +0100

    Added image as an option for posts

--- server.js
+++ server.js
@@ -159,7 +159,8 @@ app.get('/blog/:post', function (req, res) {
     var postData = fm(fs.readFileSync(pathToPost) + '')
     var link = slugify(files[0])
     var title = postData.attributes.title
-    var desc = postData.body.slice(0,320)
+    var image = postData.attributes.image || '/images/logo/logo.png'
+    var desc = postData.body.slice(0, 320)
 
     res.send(`<!doctype html>
 <html>
@@ -173,7 +174,7 @@ app.get('/blog/:post', function (req, res) {
     <meta name="author" content="Ascend NTNU" />
     <meta property="fb:app_id" content="202744680073731" />
     <meta property="og:type" content="article" />
-    <meta property="og:image" content="https://ascendntnu.no/images/logo/logo.png" />
+    <meta property="og:image" content="https://ascendntnu.no${image}" />
     <meta property="og:title" content="Ascend NTNU - ${title}" />
     <meta property="og:description" content="${desc}..." />
     <meta property="og:url" content="https://ascendntnu.no/blog/${link}" />
