9ef2a77686e6d06e5d34c83dfa88a1e4c5e431e8
Niklas Molnes Hole <nikasmh@hotmail.com>
2017-08-07 23:01:05 +0200

    Added more OOP with types to drone page

--- app/components/DronePage/index.tsx
+++ app/components/DronePage/index.tsx
@@ -9,17 +9,45 @@ export interface DronePageState {
   droneImages: any
 }
 
+type Content = JSX.Element[]
+
+interface Model {
+  name: string,
+  parts: string[],
+}
+
+interface Drone {
+  name: string,
+  version: string,
+  content: Content,
+  model?: Model,
+  modelCollection?: Model[],
+  image: string | JSX.Element,
+  imageCollection?: string[] | JSX.Element[],
+  testDrone?: Drone,
+}
+
+interface Year {
+  year: number,
+  drone: Drone,
+}
+
 export class DronePage extends React.Component<DronePageProps, DronePageState> {
-  drones: any[]
-  years: any
+  drones: Drone[]
+  years: Year[]
 
   constructor () {
     super()
 
     this.drones = [
       {
-        name: 'Drone 2',
-        models: ['/images/drones/drone2.stl', '/images/drones/propell.stl'],
+        name: 'Drone 1.0',
+        version: 'v1.0',
+        model: {
+          name: 'Drone 2',
+          parts: ['/images/drones/drone2.stl', '/images/drones/propell.stl'],
+        },
+        imageCollection: [],
         image: <div className="drone-image" style={ { backgroundImage: 'url(/images/drones/drone2-minimized.jpg)' } }></div>,
         //image: <div className="drone-model"><ModelRenderer models={['/images/drones/drone2.stl', '/images/drones/propell.stl']} process={[-80, 160, 160]} /></div>,
         content: [
@@ -29,7 +57,8 @@ export class DronePage extends React.Component<DronePageProps, DronePageState> {
           <span>Our <b>software</b> can recognize the lines and corners of the grid, detect target robots and decide when and how to interact with them.</span>,
         ],
         testDrone: {
-          name: 'Drone 1',
+          name: 'Drone 0.0.1',
+          version: 'v0.0.1',
           image: <div className="drone-image" style={ { backgroundImage: 'url(/images/drones/drone1-flying-minimized.jpg)' } }></div>,
           content: [
             <span>Our first aerial robot was born fall of 2015. Creating it, flying it and using it has been a great learning experience for the team. The drone is however quite small, and we needed an upgrade in order to carry all the desired equipment.</span>,
@@ -37,13 +66,13 @@ export class DronePage extends React.Component<DronePageProps, DronePageState> {
         }
       },
       {
-        name: 'Drone 3',
+        name: 'Valkyrie',
+        version: 'v2.0',
         image: <div className="drone-image" style={ { backgroundImage: 'url(/images/drones/drone3-minimized.jpg)' } }></div>,
         content: [
           <span>Our third quadcopter was designed as a physically robust platform for testing new control software. The small size and low weight means that we can test new control strategies, including landing on ground robots, with less risk of damage to equipment.</span>,
           <span>It has a Pixhawk flight controller, an Odroid XU4 onboard computer running Ubuntu Server with ROS, and is used with an Optitrack tracking system. The custom made frame made of carbon fiber and 3D-printed parts allows compact placement of the hardware and even weight distribution, and well balanced motors from T-Motor minimize vibrations and yields high efficiency.</span>,
         ],
-        testDrone: null
       }
     ]
 
@@ -51,12 +80,10 @@ export class DronePage extends React.Component<DronePageProps, DronePageState> {
       {
         year: 2016,
         drone: this.drones[0],
-        testDrone: this.drones[0].testDrone
       },
       {
         year: 2017,
-        drone: null,
-        testDrone: this.drones[1]
+        drone: this.drones[1],
       },
     ]
 
@@ -64,22 +91,45 @@ export class DronePage extends React.Component<DronePageProps, DronePageState> {
   }
 
   render () {
-    let drones: any = this.years.map((yearContent: any, i: number) => {
+    let drones: JSX.Element[] = this.years.map((yearContent: Year, i: number) => {
       let year: number = yearContent.year
-      let drone: any = yearContent.drone
-      let testDrone: any = yearContent.testDrone
+      let drone: Drone = yearContent.drone
+      let testDrone: Drone = drone.testDrone
 
-      let content: any = null
-      if (drone) {
-        content = drone.content.map((e: any, n: number) => {
-          return <p className="drone-text" key={n}>{e}</p>
+      let content: Content = null
+      if (drone.content) {
+        content = drone.content.map((e: JSX.Element, n: number) => <p className="drone-text" key={n}>{e}</p>)
+      }
+
+      let images: JSX.Element[] = null
+      if (drone.imageCollection) {
+        images = (drone.imageCollection as any[]).map((e: any, n: number) => {
+          return <div className="drone-gallery-image">{e}</div>
         })
       }
 
+      let image: JSX.Element
+      if (typeof drone.image === "JSX.Element") {
+        image = drone.image
+      } else {
+        image = drone.image
+      }
+
+      //    {this.state.droneImages[i] || null}
       return (
-        <SubSection key={i} title={year.toString()} className="drone-container">
-          {this.state.droneImages[i] || null}
-          {content}
+        <SubSection key={i} className="drone-container">
+          <div className="drone-main">
+            <div className="drone-image">{drone.image}</div>
+            <div className="drone-title"></div>
+          </div>
+          <div className="drone-more">
+            <div className="drone-content">
+              {content}
+            </div>
+            <div className="drone-gallery">
+              {images}
+            </div>
+          </div>
         </SubSection>
       )
     })
