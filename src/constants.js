922c5a6940ad157dbca57d6990d01d0f5f25a1ec
Anders Hopland <andershopl@hotmail.com>
2018-01-29 20:07:39 +0100

    Making migrations when creating dockerfile

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
 export const API_HOST = process.env.REACT_APP_API_HOST || ''
-export const API_ROOT = process.env.REACT_APP_API_ROOT || 'api/v1'
+export const API_ROOT = process.env.REACT_APP_API_ROOT || 'api/v2'
 export const API_URL = `${API_HOST}/${API_ROOT}`
