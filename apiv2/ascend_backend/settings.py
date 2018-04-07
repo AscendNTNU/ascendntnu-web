100aacc25156b1cb452aeb5b1e3abc555fe62bd9
anders-hopland <andershopl@hotmail.com>
2018-04-07 17:01:08 +0200

    Update settings.py

--- apiv2/ascend_backend/settings.py
+++ apiv2/ascend_backend/settings.py
@@ -50,7 +50,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
