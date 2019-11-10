378465378204670d01e1493cc043e4dfa20fe7e7
premkumar kathiravelu <bharatpremkumar@gmail.com>
2019-11-10 12:47:05 +0100

    contact update

--- src/components/ContactPage/index.js
+++ src/components/ContactPage/index.js
@@ -104,7 +104,7 @@ export class ContactInfo extends Component {
           </div>
 
           <div class="service-box">
-            <a href="/images/assets/Nyhetsbrev_finalfinal.pdf">
+            <a src={ASSETS_URL + '/images/assets/Nyhetsbrev_finalfinal.pdf'}>
               <div class="service-icon">
                 <i class="fas fa-newspaper"></i>
               </div>
