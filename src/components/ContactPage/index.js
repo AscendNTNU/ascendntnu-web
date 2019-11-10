b9f1fc6c913e548e7caaf94cbdfee7264fffe693
premkumar kathiravelu <bharatpremkumar@gmail.com>
2019-11-10 12:51:12 +0100

    drone update

--- src/components/ContactPage/index.js
+++ src/components/ContactPage/index.js
@@ -104,7 +104,11 @@ export class ContactInfo extends Component {
           </div>
 
           <div class="service-box">
-            <a src={ASSETS_URL + '/images/assets/Nyhetsbrev_finalfinal.pdf'}>
+            <a
+              herf="
+              src={ASSETS_URL + '/images/assets/Nyhetsbrev_finalfinal.pdf'}
+              "
+            >
               <div class="service-icon">
                 <i class="fas fa-newspaper"></i>
               </div>
